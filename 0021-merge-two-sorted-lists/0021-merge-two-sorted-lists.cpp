0f8eb41654fc007fd0d2aea486cf12e700c57e85
Purva <99363043+purva5598@users.noreply.github.com>
2024-08-27 00:05:19 +0530

    Time: 9 ms (12.66%), Space: 24.7 MB (8.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -9,34 +9,23 @@
  * };
  */
 class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int> temp;
-        ListNode* node=new ListNode(-1);
-        node=list1;
-        while(node!=NULL){
-            
-            temp.push_back(node->val);
-            node=node->next;
+    ListNode* f(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) return list2; // If list1 is exhausted, return list2
+        if (list2 == nullptr) return list1; // If list2 is exhausted, return list1
+
+        ListNode* result = nullptr;
+        if (list1->val <= list2->val) {
+            result = new ListNode(list1->val); // Create a new node with list1's value
+            result->next = f(list1->next, list2); // Recursively merge the rest
+        } else {
+            result = new ListNode(list2->val); // Create a new node with list2's value
+            result->next = f(list1, list2->next); // Recursively merge the rest
         }
-        node=list2;
-        while(node!=NULL){
-            
-            temp.push_back(node->val);
-            node=node->next;
-        }
-        sort(temp.begin(),temp.end());
-        
-        ListNode* head=new ListNode(-1);
-        ListNode* ptr=head;
-        
-        for(int i=0;i<temp.size();i++){
-          ListNode* nn=new ListNode(temp[i]);
-            ptr->next=nn;
-            ptr=ptr->next;
-        }
-        
-        return head->next;
+        return result;
     }
     
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        return f(list1, list2); // Start the recursive merging
+    }
 };
